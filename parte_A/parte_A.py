5036fc39e7771a8c60f6b270cefc3ca1b233419e
cerottonicotina <cerottonicotina@gmail.com>
2026-05-22 22:05:58 +0200

    Update authors in parte_A.py

--- parte_A/parte_A.py
+++ parte_A/parte_A.py
@@ -1,6 +1,6 @@
 # =============================================================================
 # CASO PRATICO EMFI 1 – PARTE A: Frontiera Efficiente
-# Autori: Leonardo Pratelli – Università di Pisa
+# Autori: Leonardo Pratelli, Sara Albotica – Università di Pisa
 # =============================================================================
 #
 # OBIETTIVO
